255c77e64cddc799820fe8f2ebcf4a5b384f2f8e
paul <paul@socialmoth.com>
2008-10-23 01:05:34 -0700

    move code back in to top object

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -68,7 +68,15 @@ public
     respond_to do |format|
       if @asset.save
         flash[:notice] = 'Asset was successfully created.'
-        format.html { redirect_to asset_url(@asset) }
+        format.html { 
+          if params[:through_iframe]
+            responds_to_parent do
+              render :template => "photos/create.js.rjs"
+            end
+          else
+            redirect_to asset_url(@asset)
+          end
+        }
         format.xml  { head :created, :location => asset_url(@asset) }
       else
         format.html { render :action => "new" }
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,29 +3,6 @@ class PhotosController < UserAssetsController
   delegate_resources_helpers :assets, :to => :photos, :controller => :photos
   delegate_url_helpers :asset_attachable, :to => :user
 
-  def create
-    @asset = assets.build(params[:asset])
-
-    respond_to do |format|
-      if @asset.save
-        flash[:notice] = 'Asset was successfully created.'
-        format.html { 
-          if params[:through_iframe]
-            responds_to_parent do
-              render :template => "photos/create.js.rjs"
-            end
-          else
-            redirect_to asset_url(@asset)
-          end
-        }
-        format.xml  { head :created, :location => asset_url(@asset) }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @asset.errors.to_xml }
-      end
-    end
-  end
-
 protected
   def load_attachable
     # We do both @user and @attachable because @attachable is used by the assets_controller
