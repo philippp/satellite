e5e7cd86005510dce347d551df7b51c1328d9ba3
Philipp Pfeiffenberger <phil@deluux.com>
2008-10-24 05:55:15 -0500

    removing tags is a plus

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,8 +1,9 @@
 class TagsController < ApplicationController
   
   before_filter :load_user_asset
-  
+  before_filter :load_tag, :only => [:destroy]
   def create
+    @asset.reload
     @tag = Tag.new(:x => params[:x],
                    :y => params[:y],
                    :asset => @user_asset);
@@ -20,7 +21,7 @@ class TagsController < ApplicationController
             page << "photo_tagger.reload_tags();"
             page << "photo_tagger.show_tags();"
             page << "photo_tagger.cancelForm();"
-            page << "update_tag_list('#{name}');"
+            page.replace_html "tag-list", :partial => "tag_list", :locals => { :asset => @asset}
             page["create-tag-response"].update("Created tag for #{@friend.name}")
 
           }}
@@ -38,6 +39,25 @@ class TagsController < ApplicationController
     end
 
   end
+  
+  def destroy
+    
+    respond_to do |format|
+    tag_id = @tag.id
+      if @tag.destroy
+        format.js   { render(:update){ |page|
+            page.remove "tag-list-item-#{tag_id}" 
+            page.remove "pt-tag-#{tag_id}"
+          }
+        }
+      else
+        format.js   { render(:update){ |page|
+            page["tag-list-item-#{tag_id}"].update("Could not remove tag for #{tag.friend.name}") 
+          }
+        }
+      end
+    end
+  end
 
   def index
   end
@@ -47,5 +67,8 @@ class TagsController < ApplicationController
     @asset = @user_asset # apparently there is a lot of stupid code assuming we have both an @asset and @user_asset
   end
 
+  def load_tag
+    @tag = Tag.find(params[:id]) || Tag.find(params[:tag_id]) 
+  end
 
 end
